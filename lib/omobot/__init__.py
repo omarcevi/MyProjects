ecc1d29dd2188dce407edb192bd7b4d8c5389ba7
Omar <omar680ag@gmail.com>
2020-12-15 22:15:50 +0300

    added embeds

--- lib/omobot/__init__.py
+++ lib/omobot/__init__.py
@@ -1,3 +1,6 @@
+from discord import Intents
+from discord import Embed
+from datetime import datetime
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from discord.ext.commands import Bot as BotBase 
 
@@ -10,13 +13,20 @@ class OMOBot(BotBase):
         self.ready = False
         self.guild = False
         self.scheduler = AsyncIOScheduler()
-        super().__init__(command_prefix= PREFIX, owner_ids= OWNER_IDS)
+        
+        super().__init__(
+            command_prefix= PREFIX, 
+            owner_ids= OWNER_IDS,
+            intents = Intents.all()
+            )
 
     def run(self, version):
         self.VERSION = version
         
-        with open("./lib/omobot/token", 'r', encoding= 'utf=8') as tf:
+        with open("./lib/omobot/token.0", 'r', encoding= 'utf=8') as tf:
             self.TOKEN = tf.read()
+        print('OMOBot is running...')
+        super().run(self.TOKEN, reconnect= True)
         
 
     async def on_connect(self):
@@ -30,6 +40,28 @@ class OMOBot(BotBase):
             self.ready = True
             self.guild = self.get_guild(785445213442932736)
             print('OMOBot Ready')
+        
+            channel = self.get_channel(788470589421649920)
+
+            await channel.send('OMOBot Online!')
+
+            embed = Embed(title= "Now Online!", description= "OMOBot is now Online and ready",
+                        colour=0xFF0000, timestamp=datetime.utcnow())
+
+            
+            fields = [("Name", "Value", True),
+                      ("Another field", "This field is next to the other one.", True),
+                      ("A non-inline field", "This field will appear on it's own row.", False)]
+            for name, value, inline in fields:
+                embed.add_field(name=name, value=value, inline=inline)
+            
+            embed.set_author(name="Omos", icon_url=self.guild.icon_url)
+            embed.set_footer(text="This is a footer!")
+            embed.set_thumbnail(url= self.guild.icon_url)
+            embed.set_image(url= self.guild.icon_url)
+            await channel.send(embed=embed)
+
+
         else:
             print('OMOBot is back')
 
