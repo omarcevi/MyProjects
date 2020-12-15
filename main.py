9cec4cc9532e9fd605d24b40b6b73a9fb72e628b
Omar <omar680ag@gmail.com>
2020-12-15 16:17:20 +0300

    first code

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 # First file for OMOBot
 
 import discord 
+from dotenv import load_dotenv
+import os
 
+
+load_dotenv('.env')
 omobot = discord.Client()
 
 
 
+omobot.run(os.getenv('OMOBOT_TOKEN'))
